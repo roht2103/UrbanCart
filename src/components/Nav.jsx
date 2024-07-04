2bfdbcdd3997356879d5dfb4fbda2970ebc19a07
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-07-04 18:40:44 +0530

    added log in sign up using auth0

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,51 @@ import { IoSearch } from "react-icons/io5";
 import { IoIosCart } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Popover } from "antd";
 const Nav = () => {
   const [cartCount, setCartCount] = useState(0);
+  const { loginWithRedirect, logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  console.log(user);
   const searchItem = () => {
     alert("searched");
   };
+  const content = (
+    <div>
+      {!isAuthenticated && (
+        <section className="w-60 flex justify-center ">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              loginWithRedirect();
+            }}
+            className="bg-yellow-300 p-2 rounded-md w-40"
+          >
+            Sign in
+          </button>
+        </section>
+      )}
+      <section className="w-60 p-2">
+        <hr />
+        <h1 className="font-bold font-2xl">Your Account</h1>
+        <h5 className="cursor-pointer hover:text-red-600 hover:underline">
+          Account
+        </h5>
+        <h5 className="cursor-pointer hover:text-red-600 hover:underline">
+          Orders
+        </h5>
+        {isAuthenticated && (
+          <h5
+            className="cursor-pointer hover:text-red-600 hover:underline"
+            onClick={() => logout()}
+          >
+            Sign Out
+          </h5>
+        )}
+      </section>
+    </div>
+  );
   return (
     <div className="p-3 font-bold h-20 bg-black flex items-center justify-between">
       <img className="h-16 cursor-pointer" src={logo} alt="" />
@@ -35,10 +75,21 @@ const Nav = () => {
         </label>
         <input className="hidden" type="submit" id="search" />
       </form>
-      <section className="text-white cursor-pointer">
-        <p className="text-xs">Hello, sign in</p>
-        <p className="text-lg">Account</p>
-      </section>
+      <Popover content={content} trigger="hover">
+        {/* <Button>Hover me</Button> */}
+        <section
+          onClick={(e) => {
+            e.preventDefault();
+            !isAuthenticated && loginWithRedirect();
+          }}
+          className="text-white cursor-pointer"
+        >
+          <p className="text-xs">
+            Hello, {!isAuthenticated ? "sign in" : user.name}
+          </p>
+          <p className="text-lg">Account & Lists</p>
+        </section>
+      </Popover>
       <span className="flex items-center justify-center gap-1 cursor-pointer">
         <IoIosCart style={{ color: "white", fontSize: "40px" }} />
         <label className="text-white text-2xl font-light cursor-pointer">
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-2r2nzzpyro38wmjh.us.auth0.com"
+      clientId="797mvFkNVh4x7cDQpl5M3ZpwxilP5Kkh"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
