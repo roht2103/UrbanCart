24b2df898113eb3afbd9be44a3c5fc4213256286
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2024-07-25 00:08:06 +0530

    updated UI of OrdersPage

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,9 +1,11 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoHeartOutline } from "react-icons/io5";
+import { MdDelete } from "react-icons/md";
+import axios from "axios";
 import { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import Lottie from "lottie-react";
 import animation from "../assets/animations/loading.json";
-import { Link, useNavigate } from "react-router-dom";
-
 const list = [
   {
     id: 4,
@@ -74,20 +76,65 @@ const list = [
     },
   },
 ];
+const EmptyOrders = ({ isAuthenticated, loginWithRedirect }) => {
+  return (
+    <div className="bg-gray-100 m-5 px-3 py-10 flex items-center gap-4">
+      <div>
+        {isAuthenticated ? (
+          <>
+            <h1 className="text-4xl mb-2">
+              Looks like you haven't placed any orders yet.
+            </h1>
+            <p className="text-lg mb-2">
+              Once you place an order, you'll see your order history here. You
+              can track, return, or buy again from your order list.
+            </p>
+            <p className="text-lg">
+              Continue shopping on{" "}
+              <Link to="/" className="text-blue-500 hover:underline">
+                UrbanCart
+              </Link>
+              .
+            </p>
+          </>
+        ) : (
+          <>
+            <h1 className="text-4xl mb-2">Your UrbanCart Orders</h1>
+            <p className="text-lg mb-2">Sign in to see your orders.</p>
+            <button
+              onClick={() => loginWithRedirect()}
+              className="bg-yellow-300 p-2 rounded-md text-lg mt-2"
+            >
+              Sign in
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-const OrdersPage = (props) => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
-  const [orderList, setOrderList] = useState(list);
+const OrdersPage = () => {
+  const [orderList, setOrderList] = useState([]);
+  const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
   const navigate = useNavigate();
-  // useEffect(() => {
-  //   if (
-  //     props.currentUser &&
-  //     props.currentUser.orders &&
-  //     props.currentUser.orders.length > 0
-  //   ) {
-  //     setOrderList(props.currentUser.orders);
-  //   }
-  // }, [props.currentUser]);
+
+  const fetchOrders = async (email) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:1000/user?email=${email}`
+      );
+      setOrderList(response.data[0].orders); // Assuming 'orders' is the correct field
+    } catch (error) {
+      console.error("There was an error fetching the orders!", error);
+    }
+  };
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      fetchOrders(user.email);
+    }
+  }, [isAuthenticated, user, isLoading]);
 
   if (isLoading) {
     return (
@@ -98,72 +145,48 @@ const OrdersPage = (props) => {
   }
 
   return (
-    <div>
-      <h1 className="text-4xl m-5 mb-0 px-3">Your Orders</h1>
-      <hr className="md:m-5 m-1" />
-
-      {isAuthenticated && orderList.length === 0 ? (
-        <div className="m-5 px-3 py-10">
-          <h1 className="text-4xl mb-2">
-            Looks like you haven&apost placed any orders yet.
-          </h1>
-          <p>
-            Once you place an order, you'll see your order history here. You can
-            track, return, or buy again from your order list.
-          </p>
-          <p className="text-center mt-10 my-2">
-            Continue shopping on{" "}
-            <Link className="text-blue-700 underline" to="/">
-              UrbanCart
-            </Link>{" "}
-            to find items you love.
-          </p>
-        </div>
+    <div className="bg-gray-100 p-5">
+      {orderList.length === 0 ? (
+        <EmptyOrders
+          isAuthenticated={isAuthenticated}
+          loginWithRedirect={loginWithRedirect}
+        />
       ) : (
-        <div className="md:m-5 px-3 py-10">
-          {isAuthenticated ? (
-            orderList.map((product) => (
-              <div
-                key={product.id}
-                className="bg-gray-100 flex justify-between flex-wrap gap-5 mb-3 p-5 cursor-pointer z-0"
-                onClick={() =>
-                  navigate("/account/orders/order-details", { state: product })
-                }
-              >
-                <div className="flex flex-wrap gap-5">
-                  <div className="flex-shrink-0">
-                    <img
-                      src={`..\\${product.src}`}
-                      className="w-40 h-40 object-cover"
-                      alt={product.name}
-                    />
-                  </div>
-                  <div className="flex flex-col justify-between">
-                    <div>
-                      <p className="text-2xl font-semibold">{product.name}</p>
-                      <p className="text-gray-600 mt-2">
-                        {product.description}
-                      </p>
-                      <p className="text-gray-600">
-                        Category: {product.category}
-                      </p>
-                    </div>
-                    <div className="flex items-center mt-4">
-                      <p className="text-lg font-bold">{product.price} $</p>
-                    </div>
-                  </div>
+        <div className="flex flex-col gap-1 bg-gray-100">
+          <h1 className="bg-white text-4xl p-3">My Orders</h1>
+          {orderList.map((order) => (
+            <div
+              key={order.id}
+              className="bg-white flex justify-between flex-wrap gap-5 p-5 cursor-pointer z-0"
+              onClick={() =>
+                navigate("/account/orders/order-details", { state: order })
+              }
+            >
+              <div className="flex flex-wrap gap-5">
+                <div className="flex-shrink-0">
+                  <img
+                    src={`..\\${order.src}`}
+                    className="w-40 h-40 object-cover"
+                    alt={order.name}
+                  />
                 </div>
-                <div>
-                  <p className="font-bold">
-                    Delivered on {product.deliveryDate}
-                  </p>
-                  <p className="text-xs">Your item has been delivered</p>
+                <div className="flex flex-col justify-between">
+                  <div>
+                    <p className="text-2xl font-semibold">{order.name}</p>
+                    <p className="text-gray-600 mt-2">{order.description}</p>
+                    <p className="text-gray-600">Category: {order.category}</p>
+                  </div>
+                  <div className="flex items-center mt-4">
+                    <p className="text-lg font-bold">{order.price} $</p>
+                  </div>
                 </div>
               </div>
-            ))
-          ) : (
-            <h1>sign in</h1>
-          )}
+              <div>
+                <p className="font-bold">Delivered on {order.deliveryDate}</p>
+                <p className="text-xs">Your item has been delivered</p>
+              </div>
+            </div>
+          ))}
         </div>
       )}
     </div>
